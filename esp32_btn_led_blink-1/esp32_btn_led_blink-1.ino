164f6936d10231713453fed8da745cb268170762
rsp <siripokarpirom@gmail.com>
2018-05-28 22:49:44 +0700

    Add files via upload

---
+++ esp32_btn_led_blink-1/esp32_btn_led_blink-1.ino
@@ -0,0 +1,57 @@
+// IoT Engineering Education @ KMUTNB, Bangkok, Thailand
+// Date: 28-May-2018
+
+#include <freertos/task.h>
+
+#define LED_PIN    5  // GPIO-5 (onboard LED on Lolin32)
+#define BTN_PIN   32  // GPIO-32 (connected to an external push button, active-low)
+
+#define DEBOUNCE_TIME_TICKS  100
+
+volatile bool btn_pressed = false;
+volatile uint32_t last_ticks = 0;
+
+int led_state = 0;
+uint32_t click_cnt = 0;
+boolean save_pressed;
+
+portMUX_TYPE mux = portMUX_INITIALIZER_UNLOCKED;
+
+void btn_isr_callback( void ) {
+   uint32_t ticks = xTaskGetTickCount(); // get the timer ticks
+   portENTER_CRITICAL_ISR( &mux ) ; // enter critical section (within ISR)
+   if ( ticks - last_ticks >= DEBOUNCE_TIME_TICKS ) {
+      btn_pressed = true; 
+   }
+   last_ticks = ticks;
+   portEXIT_CRITICAL_ISR( &mux );  // leave critical section (within ISR)
+}
+
+void setup() {
+   Serial.begin( 115200 );
+   Serial.printf( "\n\n\n\n" );
+   
+   pinMode( BTN_PIN, INPUT_PULLUP );
+   attachInterrupt( digitalPinToInterrupt(BTN_PIN), btn_isr_callback, FALLING );
+   pinMode( LED_PIN, OUTPUT );
+   digitalWrite( LED_PIN, led_state );
+   last_ticks = xTaskGetTickCount(); 
+}
+
+void loop() {
+   portENTER_CRITICAL( &mux );   // enter critical section
+   if ( !save_pressed && btn_pressed ) {
+      save_pressed = btn_pressed;
+   }
+   btn_pressed = false;
+   portEXIT_CRITICAL( &mux );   // leave critical section
+
+   if ( save_pressed && (digitalRead( BTN_PIN) == HIGH ) ) {
+      save_pressed = false;   
+      led_state ^= 1;                        // toggle the LED state
+      digitalWrite( LED_PIN, led_state );    // update the LED output
+      Serial.printf( "Button clicked... (%d)\n", click_cnt++ );  // show the click count as a text string 
+   }
+   vTaskDelay( 10 / portTICK_PERIOD_MS );
+}
+
---
+++ esp32_btn_led_blink-2/esp32_btn_led_blink-2.ino
@@ -0,0 +1,42 @@
+// IoT Engineering Education @ KMUTNB, Bangkok, Thailand
+// Date: 28-May-2018
+
+#define LED_PIN    5  // GPIO-5  (onboard LED on Lolin32)
+#define BTN_PIN   32  // GPIO-32 (connected to an external push button, active-low)
+
+int led_state = 0;
+uint32_t click_cnt = 0;
+
+SemaphoreHandle_t xSemaphore = NULL;
+
+portMUX_TYPE mux = portMUX_INITIALIZER_UNLOCKED;
+
+void btn_isr_callback( void ) {
+   portENTER_CRITICAL_ISR( &mux ); // enter critical section
+   detachInterrupt( digitalPinToInterrupt(BTN_PIN) );
+   xSemaphoreGiveFromISR( xSemaphore, NULL );
+   portEXIT_CRITICAL_ISR( &mux );  // leave critical section
+}
+
+void setup() {
+   Serial.begin( 115200 );
+   Serial.printf( "\n\n\n\n" );
+   
+   pinMode( BTN_PIN, INPUT_PULLUP );
+   attachInterrupt( digitalPinToInterrupt(BTN_PIN), btn_isr_callback, FALLING );
+   pinMode( LED_PIN, OUTPUT );
+   digitalWrite( LED_PIN, led_state );
+
+   xSemaphore = xSemaphoreCreateBinary(); // create a binary semaphore
+}
+
+void loop() {
+   if ( xSemaphoreTake( xSemaphore, portMAX_DELAY) == pdTRUE ) { // blocked if the button is not pressed
+      while ( digitalRead( BTN_PIN ) == LOW ) { vTaskDelay( 5 ); } // wait until the button is released
+      led_state ^= 1;                        // toggle the LED state
+      digitalWrite( LED_PIN, led_state );    // update the LED output
+      Serial.printf( "Button click... (%d)\n", click_cnt++ );  // show the click count as a text string 
+      attachInterrupt( digitalPinToInterrupt(BTN_PIN), btn_isr_callback, FALLING ); // re-enable external interrupt
+   }
+}
+
