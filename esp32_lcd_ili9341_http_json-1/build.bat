2f0353148d8129ddca4f71c0e3d052991a673522
rsp <siripokarpirom@gmail.com>
2019-03-06 12:26:44 +0700

    Add files via upload

---
+++ esp32_lcd_ili9341_http_json-1/build.bat
@@ -0,0 +1,9 @@
+@echo off
+set ARDUINO_CLI_DIR=C:\Work\Arduino_CLI
+set SKETCH_DIR=C:\Work\Arduino_CLI
+set SKETCH_NAME=esp32_lcd_ili9341_http_json-1
+set SKETCH_FULLNAME=%SKETCH_DIR%\%SKETCH_NAME%
+echo "Build '%SKETCH_FULLNAME%'..."
+%ARDUINO_CLI_DIR%\arduino-cli.exe compile --config-file %SKETCH_DIR%\.cli-config.yml --fqbn esp32:esp32:esp32 %SKETCH_FULLNAME%
+echo "Done..."
+pause
---
+++ esp32_lcd_ili9341_http_json-1/upload.bat
@@ -0,0 +1,14 @@
+@echo off
+:: the first argument is the COM port
+set SKETCH_DIR=C:\Work\Arduino_CLI
+set ARDUINO_ESP32_PATH=C:\Users\%USERNAME%\AppData\Local\Arduino15\packages\esp32\hardware\esp32\1.0.1
+set PORT=%1
+set SKETCH_NAME=esp32_lcd_ili9341_http_json-1
+set SKETCH_FULLNAME=%SKETCH_DIR%\%SKETCH_NAME%
+echo "Upload '%SKETCH_FULLNAME%'..."
+:: esptool.py -p %PORT% flash_id
+:: esptool.py --chip esp32 -b 921600 -p %PORT% erase_flash
+%ARDUINO_ESP32_PATH%\tools\esptool.py --chip esp32 -b 921600 -p %PORT% --before default_reset --after hard_reset  write_flash -z --flash_mode dio --flash_freq 80m --flash_size=detect 0xe000 %ARDUINO_ESP32_PATH%\tools\partitions\boot_app0.bin  0x1000  %ARDUINO_ESP32_PATH%\tools\sdk\bin\bootloader_dio_80m.bin  0x10000 "%SKETCH_FULLNAME%\%SKETCH_NAME%".esp32.esp32.esp32.bin
+
+echo "Done..."
+pause
